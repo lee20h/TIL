1959a03e8f52c1f1116c87615c85b0ea2440f80a
Young Hun <lee0h.0417@gmail.com>
2020-08-05 00:17:56 +0900

    test

---
+++ depoly.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+cd -
