7a9ad1eead69bd90e9c4f15483ebbd2c59b8ef70
Young Hun <lee0h.0417@gmail.com>
2021-03-01 22:35:40 +0900

    2021-03-01

---
+++ Problem Solving/LeetCode/Distribute Candies.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set<int> s;
+        for(int i=0; i<candyType.size(); i++) {
+            s.insert(candyType[i]);
+        }
+        return min(s.size(), candyType.size()/2);
+    }
+};
