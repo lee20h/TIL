224d977d3436b627e3baa76807547525f10666fe
Young Hun <lee0h.0417@gmail.com>
2021-09-25 23:50:04 +0900

    2021-09-25

--- Problem Solving/LeetCode/N-th Tribonacci Number.go
+++ Problem Solving/LeetCode/N-th Tribonacci Number.go
@@ -1,11 +1,12 @@
 package leetcode
+
 func tribonacci(n int) int {
 	if n < 2 {
 		return n
 	}
 	cur, prev, prev2 := 1, 1, 0
 	for n > 2 {
-		cur, prev, prev2 = cur + prev + prev2, cur, prev
+		cur, prev, prev2 = cur+prev+prev2, cur, prev
 		n--
 	}
 	return cur
